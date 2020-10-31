b83473311629141a667a502e93975a241e4f78ae
Luke Eastman <LukeAEastman@gmail.com>
2020-10-31 12:13:17 -0700

    update package.json to properly watch files with webpack

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('HELLO')
-});
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
